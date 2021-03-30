1dfbce0cd2cf3aabb5a9823298aa4489ceab9d1f
Cyril Dumont <cyril.dumont@leuville.com>
2021-03-30 09:47:02 +0200

    hello sans accent

--- hello.sh
+++ hello.sh
@@ -2,7 +2,7 @@
 WORD="World"
 [ 0 -lt $# ] && WORD="$@"
 echo "Hello, $WORD"
-echo "Programme développé par :"
+echo "Programme developpe par :"
 while read i; do
   echo "* $i"
 done < MAINTAINERS.txt
